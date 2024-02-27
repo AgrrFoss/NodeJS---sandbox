ccd3359810c3c69fddee66920a7356b468af8dd2
Gregori <orlov.gregori@yandex.ru>
2024-02-27 20:09:46 +0300

    homework for async/await

--- project_01_cjs/index.js
+++ project_01_cjs/index.js
@@ -1,3 +1,73 @@
 const Square = require('./square.js');
 const o = new Square(5);
 console.log(`square = ${o.area()}`);
+
+
+//assync  examples (cjs)
+
+const p1 = new Promise ((resolve, reject) => {
+    setTimeout(()=> {
+        console.log('timeout finished');
+        if(Boolean(+((new Date()).getSeconds())%2)){
+            resolve('success');
+        } else {
+            reject('error');
+        }
+    }, 2000)
+});
+// classic work with promise
+p1.then(state=> {
+    console.log('then detected', state);
+}).catch(err=>{
+    console.log('catch detected', err);
+}).finally(()=> {
+    console.log('finally detected');
+});
+
+// Don't work in cjs
+/*
+try{
+    const  successResult = await p1;
+    console.log('Async call success', successResult);
+} catch(e) {
+    console.log('Async call failed', e);
+}
+*/
+
+
+
+
+
+
+// async await in cjs
+// async function runner();
+async function runner() {
+    try{
+        const  successResult = await p1;
+        console.log('Async call success', successResult);
+    } catch(e) {
+        console.log('Async call failed', e);
+    }
+}
+runner();
+
+// working async await unname function
+(async function() {
+    try{
+        const  successResult = await p1;
+        console.log('Async call success', successResult);
+    } catch(e) {
+        console.log('Async call failed', e);
+    }
+})();
+
+// working async await arrow function
+(async ()=> {
+    try{
+        const  successResult = await p1;
+        console.log('Async call success', successResult);
+    } catch(e) {
+        console.log('Async call failed', e);
+    }
+})();
+
--- project_03_mjs_ass_js/index.js
+++ project_03_mjs_ass_js/index.js
@@ -5,3 +5,25 @@ const o = new Square(4);
 const s = new SquareCircle(8);
 console.log(`Square = ${o.area()}`);
 console.log(`Square = ${s.area()}`);
+
+
+
+// async exemple mjs
+const p1 = new Promise ((resolve, reject) => {
+    setTimeout(()=> {
+        console.log('timeout finished');
+        if(Boolean(+((new Date()).getSeconds())%2)){
+            resolve('success');
+        } else {
+            reject('error');
+        }
+    }, 2000)
+});
+
+// Work in mjs (now work in cjs)
+try{
+    const  successResult = await p1;
+    console.log('Async call success', successResult);
+} catch(e) {
+    console.log('Async call failed', e);
+}
